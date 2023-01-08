73d3de6982d33908d097e72d40cbe586b06b47f9
André Craco <49844995+andrecrax@users.noreply.github.com>
2023-01-08 18:17:20 -0300

    adicionado soma dos valores

--- script.js
+++ script.js
@@ -34,7 +34,53 @@ function finishOrder(){
     }
   }
   function sendWhatsAppMessage() {
-    var text = encodeURIComponent("Olá, gostaria de fazer o pedido:\n- Prato: Frango Yin Yang\n- Bebida: Coquinha Gelada\n- Sobremesa: Pudim\nTotal: R$ 27,70");
+    var comida = document.querySelector(".meal .selecionado h3").textContent;
+    var texto = document.querySelector(".meal .selecionado h4").innerHTML;
+var precoComida = "";
+
+for (var i = 0; i < texto.length; i++) {
+    if (/[\d,]/.test(texto[i])) {
+    if (texto[i] === "," || (texto[i] === "." && texto[i-1] !== ",")) {
+        precoComida += ",";
+      } else {
+        precoComida += texto[i];
+      }
+    }
+}
+
+    var bebida = document.querySelector(".drinks .selecionado h3").textContent;
+    var texto = document.querySelector(".drinks .selecionado h4").innerHTML;
+    var precoBebida = "";
+    
+    for (var i = 0; i < texto.length; i++) {
+        if (/[\d,]/.test(texto[i])) {
+        if (texto[i] === "," || (texto[i] === "." && texto[i-1] !== ",")) {
+            precoBebida += ",";
+          } else {
+            precoBebida += texto[i];
+          }
+        }
+    }
+    var sobremesa = document.querySelector(".dessert .selecionado h3").textContent;
+    var texto = document.querySelector(".dessert .selecionado h4").innerHTML;
+var precoSobremesa = "";
+
+for (var i = 0; i < texto.length; i++) {
+    if (/[\d,]/.test(texto[i])) {
+    if (texto[i] === "," || (texto[i] === "." && texto[i-1] !== ",")) {
+        precoSobremesa += ",";
+      } else {
+        precoSobremesa += texto[i];
+      }
+    }
+}
+precoSobremesa = precoSobremesa.replace(",",".");
+precoComida = precoComida.replace(",",".");
+precoBebida = precoBebida.replace(",",".");
+
+precoFinal = parseFloat(precoBebida) + parseFloat(precoComida) + parseFloat(precoSobremesa);
+
+    var text = encodeURIComponent("Olá, gostaria de fazer o pedido:\n- Prato: "+ comida + "\n- Bebida: " + bebida + "\n- Sobremesa: " + sobremesa + "\nTotal: R$ " + precoFinal);
     var url = "https://wa.me/1234567890?text=" + text;
     window.open(url, "_blank");
   }
